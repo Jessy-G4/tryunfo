d2d93aeffc5ea7b97b7b3ef4cf760d049d2c1a67
Jessy Cristina <jessyaraujo395@gmail.com>
2022-06-27 17:40:32 -0300

    progresso requisito 4

--- src/components/Form.js
+++ src/components/Form.js
@@ -5,97 +5,118 @@ import PropTypes from 'prop-types';
 // Jessy do futuro, lembrete, tem uma prop chamada HasTrunfo tu comentou ela para corrigir erro do lint, quando for usar, não esquece de descomentar!
 // Jessy do futuro, lembrete, tem uma prop chamada HasTrunfo tu comentou ela para corrigir erro do lint, quando for usar, não esquece de descomentar!
 class Form extends React.Component {
-  render() {
-    const { cardName,
-      cardDescription,
-      cardAttr1,
-      cardAttr2,
-      cardAttr3,
-      cardImage,
-      cardRare,
-      cardTrunfo,
-      //   hasTrunfo,
-      isSaveButtonDisabled,
-      onInputChange,
-      onSaveButtonClick,
-    } = this.props;
-    return (
-      <>
-        <input
-          type="text"
-          data-testid="name-input"
-          name="Nome"
-          placeholder="Nome da carta"
-          value={ cardName }
-          onChange={ onInputChange }
-        />
-        <textarea
-          data-testid="description-input"
-          name="descrição"
-          value={ cardDescription }
-          onChange={ onInputChange }
-        />
-        <input
-          type="number"
-          data-testid="attr1-input"
-          name="Atributo1"
-          placeholder="Insira o Primeiro Atributo"
-          value={ cardAttr1 }
-          onChange={ onInputChange }
-        />
-        <input
-          type="number"
-          data-testid="attr2-input"
-          name="Atributo2"
-          placeholder="Insira o Segundo Atributo"
-          value={ cardAttr2 }
-          onChange={ onInputChange }
-        />
-        <input
-          type="number"
-          data-testid="attr3-input"
-          name="Atributo3"
-          placeholder="Insira o Terceiro Atributo"
-          value={ cardAttr3 }
-          onChange={ onInputChange }
-        />
-        <input
-          type="text"
-          data-testid="image-input"
-          alt="Imagem Definida"
-          placeholder="Insira o URL da imagem"
-          value={ cardImage }
-          onChange={ onInputChange }
-        />
-        <select
-          name="Raridade"
-          data-testid="rare-input"
-          value={ cardRare }
-          onChange={ onInputChange }
-        >
-          <option value="normal">normal</option>
-          <option value="raro">raro</option>
-          <option value="muito raro">muito raro</option>
-        </select>
-        <input
-          type="checkbox"
-          data-testid="trunfo-input"
-          name="Tryunfo"
-          checked={ cardTrunfo }
-          onChange={ onInputChange }
-        />
-        <button
-          type="submit"
-          data-testid="save-button"
-          disabled={ isSaveButtonDisabled }
-          onClick={ onSaveButtonClick }
-        >
-          Salvar Carta
-
-        </button>
-      </>
-    );
+  constructor() {
+    super();
+    this.state = {
+      // Nome: '',
+      // descricao: '',
+      // Atributo1: 0,
+      // url: '',
+      // Raridade: '',
+      // Tryunfo: '',
+      // submit: '',
+    };
   }
+
+ handleChance = (evento) => {
+   const { name, type, checked, value } = evento.target;
+   this.setState({ [name]: type === 'checkbox' ? checked : value });
+ }
+
+ render() {
+   const {
+     cardName,
+     cardDescription,
+     cardAttr1,
+     cardAttr2,
+     cardAttr3,
+     cardImage,
+     cardRare,
+     cardTrunfo,
+     //   hasTrunfo,
+     isSaveButtonDisabled,
+     onInputChange,
+     onSaveButtonClick,
+   } = this.props;
+   return (
+     <>
+       <input
+         type="text"
+         data-testid="name-input"
+         name="Nome"
+         placeholder="Nome da carta"
+         value={ cardName }
+         onChange={ onInputChange }
+       />
+       <textarea
+         data-testid="description-input"
+         name="descricao"
+         value={ cardDescription }
+         onChange={ onInputChange }
+       />
+       <input
+         type="number"
+         data-testid="attr1-input"
+         name="Atributo1"
+         placeholder="Insira o Primeiro Atributo"
+         value={ cardAttr1 }
+         onChange={ onInputChange }
+       />
+       <input
+         type="number"
+         data-testid="attr2-input"
+         name="Atributo2"
+         placeholder="Insira o Segundo Atributo"
+         value={ cardAttr2 }
+         onChange={ onInputChange }
+       />
+       <input
+         type="number"
+         data-testid="attr3-input"
+         name="Atributo3"
+         placeholder="Insira o Terceiro Atributo"
+         value={ cardAttr3 }
+         onChange={ onInputChange }
+       />
+       <input
+         type="text"
+         name="url"
+         data-testid="image-input"
+         alt="Imagem Definida"
+         placeholder="Insira o URL da imagem"
+         value={ cardImage }
+         onChange={ onInputChange }
+       />
+       <select
+         name="Raridade"
+         data-testid="rare-input"
+         value={ cardRare }
+         onChange={ onInputChange }
+       >
+         <option value="normal">normal</option>
+         <option value="raro">raro</option>
+         <option value="muito raro">muito raro</option>
+       </select>
+       <input
+         type="checkbox"
+         data-testid="trunfo-input"
+         name="Tryunfo"
+         checked={ cardTrunfo }
+         onChange={ onInputChange }
+       />
+       <button
+         name="submit"
+         type="submit"
+         data-testid="save-button"
+         disabled={ isSaveButtonDisabled }
+         onClick={ onSaveButtonClick }
+       >
+         Salvar Carta
+
+       </button>
+     </>
+   );
+ }
 }
 
 Form.propTypes = {
